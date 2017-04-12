2c033e9e80ef0f9241a7442947b9fb70b120f749
Des TC <Des TC>
2017-04-12 08:15:40 +0300

    Initial commit

---
+++ app/src/main/java/gr/pcbugs/www/pcbugs3/MainActivity.java
@@ -0,0 +1,18 @@
+package gr.pcbugs.www.pcbugs3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
